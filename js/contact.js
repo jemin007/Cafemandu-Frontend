8a6945d7aeea8d176a1a48ad836880eae6df564b
jemin <jeminshrestha9@gmail.com>
2020-02-11 12:31:29 +0545

    contacts completed

---
+++ js/contact.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+	// Login
+	const url = 'http://localhost:3000/contacts';
+	const contactBtn = document.getElementById('contactBtn');
+	contactBtn.addEventListener('click', contactSubmit);
+
+	async function contactSubmit(event){
+		const contactEmail = document.getElementById('contactEmail').value;
+		const contactText = document.getElementById('contactText').value;
+		const data = await fetch(url,{
+			method: "POST",
+			headers:{
+				'Content-Type': 'application/json'
+			},
+			body:JSON.stringify({
+				
+				email: contactEmail,
+				description: contactText
+			})
+		})
+		const json = await data.json();
+		console.log(json);
+		if(json.status == "success"){
+			alert("Contact has been added!");
+		}else{
+			alert("Failed to post your contact!");
+		}
+	}
+}
